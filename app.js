34b5267bf6f7e1893282c6ca9f2047f1d33b9041
Steven Kauyedauty <skauyedauty@gmail.com>
2015-07-02 22:54:44 -0600

    updates

--- app.js
+++ app.js
@@ -7,20 +7,13 @@ var app= angular.module('myApp',[]);
 		this.response ={};
 	});
 
-// var app= angular.module('myApp', [])
-// .constant('Name', 'response')
-// app.controller('Naming', function($scope) {
-//   $scope.naming=Name;
-// });
-
-// Name is the constant it is reused as a argument for .run .rootScope is used to assist communication between template and block of code
+// this is my effort to add behavior to the scope.
+// var app = angular.module('myApp', []);
+// app.controller('NamingController',['$scope', function($scope){
+// $scope.customName ="response";
+// }]);
 
 
-// angular.module('myApp', [])
-//     .constant('VERSION', 'response')
-//     .controller('MyCtrl', function(VERSION, $scope) {
-//         $scope.version = VERSION;
-//     });
 
 
    
